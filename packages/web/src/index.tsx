58d580f7c9cd3912c5e655f092725cadeb945cb8
Unnit Metaliya <unnitjmetaliya@gmail.com>
2018-12-13 21:00:10 -0500

    Added ApolloProvider for making GraphQL requests

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,7 +1,15 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { ApolloProvider } from "react-apollo";
+
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+import { client } from "./apollo";
 
-ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root") as HTMLElement
+);
 registerServiceWorker();
